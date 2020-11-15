f9e19158f3b575bc7e0c12259166dbbfdca164fe
Sarath S <sarathsedu@gmail.com>
2020-11-15 17:56:13 +0530

    added form validations

--- js/formValidations.js
+++ js/formValidations.js
@@ -1,24 +1,50 @@
 function validateLoginForm() {
-    var username = document.forms["loginForm"]["username"].value;
-    var password = document.forms["loginForm"]["password"].value;
-    if (username = "") {
-        alert("Name must be filled out");
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (isEmptyOrBlank(username)) {
+        alert("Name must be filled out!");
         return false;
+    } else if(isEmptyOrBlank(password)) {
+        alert("Password can't be blank!");
+        return false;
+    } else if(password.length<8) {
+        alert("Too short of a password!");
+        return false;
+    } else {
+        // Still have to check username and password after hashing in php from db and set cookie & session id.
+        return true;
     }
 }
 
 function validateSignUpForm() {
-    var username = document.forms["signupForm"]["username"].value;
-    var password = document.forms["signupForm"]["password"].value;
-    var repassword = document.forms["signupForm"]["repassword"].value;
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var repassword = document.getElementById("repassword").value;
     if(password != repassword){
         alert("Both passwords should match!")
         return false;
-    } else if ( password.length <8 ) {
+    } else if (isEmptyOrBlank(username)) {
+        alert("Name must be filled out!");
+        return false;
+    } else if(isEmptyOrBlank(password)) {
+        alert("Password can't be blank!");
+        return false;
+    }  else if(isEmptyOrBlank(repassword)) {
+        alert("Password can't be blank!");
+        return false;
+    }  else if (password.length <8 ) {
         alert("Too short of a password!")
         return false;
     } else {
-        // verify username unique-ness.
+        // verify username unique-ness in php, insert if successful after hashing along with cookie & session id.
         return true;
+    }
+}
+
+function isEmptyOrBlank(string) {
+    if(string == "" || string === "" || !string || /^\s*$/.test(string)) {
+        return true;
+    } else{
+        return false;
     }
 }
