a211e479a0e3942085c6632a5e8423fa695ff9b1
Sarath S <sarathsedu@gmail.com>
2020-11-15 20:54:18 +0530

    added login and signup skeleton with loading progress bar.

--- php/signup.php
+++ php/signup.php
@@ -1,7 +1,8 @@
 <?php
     include_once dirname(__FILE__) . '/constants.php';
-    // check username and hash password and check from db
-    // if success, set cookie and session id and goto dashboard
+    // check if username unique 
+    // hash password 
+    // set cookie and session id and goto dashboard
     // on failure, show alert and navigate back. 
     $conn = mysqli_connect(MYSQL_SERVER_IP,MYSQL_USER_NAME,MYSQL_PWD,DB_NAME);
     if($conn){
