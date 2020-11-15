8d82f726b7a020b16e4c7d9482cc65008ae30d22
Sarath S <sarathsedu@gmail.com>
2020-11-15 11:23:50 +0530

    added resources

--- php/login.php
+++ php/login.php
@@ -1,8 +1,13 @@
 <?php
+    include_once dirname(__FILE__) . '/constants.php';
     // check username and hash password and check from db
     // if success, set cookie and goto dashboard
     // on failure, show alert and navigate back. 
     $conn = mysqli_connect(MYSQL_SERVER_IP,MYSQL_USER_NAME,MYSQL_PWD,DB_NAME);
-    echo $conn;
+    if($conn){
+        echo "success";
+    } else {
+        echo "failure";
+    }
 
 ?>
